144115a02304843e10e1ece7dad664c21856ebc3
dunglt94 <tuandung9594@gmail.com>
2024-09-11 15:12:24 +0700

    first add

---
+++ js.js
@@ -0,0 +1,9 @@
+confirm("Login")
+function getdata() {
+    let name = document.getElementById("name").value
+    let password = document.getElementById("password").value
+    document.write(name)
+    document.write(password)
+}
+
+
